0d672039eee9d815c5e8cb84953ddd9a6b70e56d
Abhi_G <devabhi.code@gmail.com>
2019-01-09 06:31:54 +0000

    two test pass. Work on other 2.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+
+  if (array.length === 0) {
+    return 0;
+  }
+  
+  return array[0] + sumItems(array.slice(1));
+  
 }
 
 module.exports = sumItems;
